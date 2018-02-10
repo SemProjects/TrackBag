006376bb96ddf7d0d2907d688405da228d3a3026
RaviKumar12354 <ravi.95044@gmail.com>
2018-02-10 20:48:14 +0530

    first commit

---
+++ app/src/main/java/com/example/ravi/bag_location/login.java
@@ -0,0 +1,318 @@
+package com.example.ravi.bag_location;
+
+
+
+        import android.app.ProgressDialog;
+        import android.content.DialogInterface;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.graphics.Color;
+        import android.net.Uri;
+        import android.support.annotation.NonNull;
+        import android.support.v7.app.AlertDialog;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageButton;
+        import android.widget.TextView;
+
+        import com.google.android.gms.auth.api.Auth;
+        import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+        import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+        import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+        import com.google.android.gms.common.ConnectionResult;
+        import com.google.android.gms.common.api.GoogleApiClient;
+
+        import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthCredential;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+        import com.google.firebase.auth.GoogleAuthProvider;
+        import com.google.firebase.auth.ProviderQueryResult;
+        import com.google.firebase.auth.UserInfo;
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.ValueEventListener;
+
+        import android.util.Log;
+        import android.widget.Toast;
+
+public class login extends AppCompatActivity implements View.OnClickListener, OnConnectionFailedListener {
+    TextView signin;
+    Button login;
+    EditText email,pass;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    GoogleApiClient mGoogleApiClient;
+    ImageButton g;
+    private static final int RC_SIGN_IN = 007;
+    private ProgressDialog mProgressDialog;
+    boolean b;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    String eemail;
+
+    void init()
+    {
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("users");
+        signin=(TextView)findViewById(R.id.tvSignup);
+        signin.setOnClickListener(this);
+        login = (Button)findViewById(R.id.btnLogin);
+        login.setOnClickListener(this);
+        email=(EditText)findViewById(R.id.editTextemail);
+        pass=(EditText)findViewById(R.id.editTextpass);
+        g=(ImageButton)findViewById(R.id.google);
+        g.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Exit")
+                .setMessage("Are you sure?")
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).setNegativeButton("no", null).show();
+    }
+    protected void firebaseGoogleAuthentication()
+    {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        // mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+        firebase();
+        firebaseGoogleAuthentication();
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("TAG", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            showProgressDialog();
+            GoogleSignInAccount acct = result.getSignInAccount();
+            firebaseAuthWithGoogle(acct);
+        }
+        else {
+            Toast.makeText(Login.this, "Google Authentication Failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithCredential:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    String email = user.getEmail();
+                    checkAccountEmailExistInFirebase(email);
+
+                } else {
+                    hideProgressDialog();
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithCredential:failure", task.getException());
+                    Toast.makeText(Login.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void checkAccountEmailExistInFirebase(String email) {
+        eemail=email;
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot topSnapshot) {
+                for (DataSnapshot snapshot: topSnapshot.getChildren()) {
+                    user USER = snapshot.getValue(user.class);
+                    System.out.println("*********User Name:" + USER.name);
+                    if (USER != null && USER.email.equals(eemail)) {
+                        b=true;
+                    }
+                    else
+                        System.out.println("**********Null data found");
+                }
+                hideProgressDialog();
+                if(b) {
+                    Intent i = new Intent(Login.this, MainActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Intent i = new Intent(Login.this, signup_2.class);
+                    startActivity(i);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e("TAG", "Failed to read user", error.toException());
+            }
+        });
+    }
+    @Override
+    public void onClick(View v) {
+        int x=v.getId();
+        if(x==R.id.tvSignup)
+        {
+            Intent i=new Intent(Login.this,signup_1.class);
+            startActivity(i);
+        }
+        if(x==R.id.btnLogin) {
+            String em = email.getText().toString();
+            String ps = pass.getText().toString();
+            if (em.matches("") || ps.matches("")) {
+                System.out.println("in" + em + ps);
+                if (em.matches("")) {
+                    Toast.makeText(Login.this, "Email Field is Empty", Toast.LENGTH_SHORT).show();
+                }
+                if (ps.matches("")) {
+                    ;
+                    Toast.makeText(Login.this, "Password Field is Empty", Toast.LENGTH_SHORT).show();
+                }
+            } else
+                signIn(email.getText().toString().trim(), pass.getText().toString().trim());
+        }
+    }
+
+    void firebase()
+    {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("tag", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("tag", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+    private void signIn(String email, String password) {
+        Log.d("Tag", "signIn:" + email);
+        final ProgressDialog progressDialog = new ProgressDialog(Login.this);
+        progressDialog.setMessage("Signing In....");
+        progressDialog.show();
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("Tag", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        progressDialog.dismiss();
+                        if (!task.isSuccessful()) {
+                            Log.d("Tag", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(Login.this, "Either Password or Email Incorrect", Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            Intent i = new Intent(Login.this, MainActivity.class);
+                            startActivity(i);
+                        }
+                    }
+
+                });
+
+    }
+    private void signOut() {
+        mAuth.signOut();
+
+    }
+
+    // @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("TAG", "onConnectionFailed:" + connectionResult);
+    }
+}
